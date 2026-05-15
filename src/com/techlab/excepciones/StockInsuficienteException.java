15c7431004644e7abcd605e84700292bc0acad77
Guillermo Seron <guillermoseron@gmail.com>
2026-05-15 19:32:09 -0300

    Corrección errores en 'excepciones'

--- src/com/techlab/excepciones/StockInsuficienteException.java
+++ src/com/techlab/excepciones/StockInsuficienteException.java
@@ -1,7 +1,5 @@
 package com.techlab.excepciones;
 
-package com.techlab.excepciones;
-
 public class StockInsuficienteException extends Exception {
     public StockInsuficienteException(String mensaje) {
         super(mensaje);
